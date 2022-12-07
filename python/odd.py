5a10be61084cf5d0cafa804e2e0f11e44e2c9c02
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2022-12-07 14:37:06 +0100

    odd.py file fully editted

--- python/odd.py
+++ python/odd.py
@@ -1,5 +1,11 @@
+print("CALCULATING ODD NUMBERS")
 odd = []
-for x in range(0, 10, 2):
-	odd.append(x+1)
+initial = int(input("Start counting from: "))
+endpoint = int(input("End at: "))
+for x in range(initial, endpoint, 2):
+	if initial%2 == 1:
+		odd.append(x+2)
+	else:
+		odd.append(x+1)
 print(odd)
 	
