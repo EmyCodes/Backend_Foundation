6c0d1ad708d89f13339087a27f8cb5fcc13f7831
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2022-12-11 10:40:17 +0100

    list_com.py updated

--- python/list_com.py
+++ python/list_com.py
@@ -1,4 +1,8 @@
 x = [num for num in range(10)]
-y = [alpha.strip(" ") for alpha in "Hello World"]
+y = [alpha.strip() for alpha in "Hello World"]
+z = [num**2 for num in x]
+zz = [num**2 for num in x if num%2 != 0]	# This expression print odd numbers
 print(x)
 print(y)
+print(z)
+print(zz)
