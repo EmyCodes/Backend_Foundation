c5886896831f1af2cbabb7a1e131ee37c79d298f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-01-29 18:08:25 +0100

    File added

---
+++ bash/script1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+
+echo "The script starts now."
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo
+w
+echo
+
+echo "I'm setting two variable now."
+COLOUR="black"
+VALUE="9"
+echo "This is a string: $COLOUR"
+echo "And this is a number: $VALUE"
+
+echo "I'm giving you back your prompt now."
+echo
