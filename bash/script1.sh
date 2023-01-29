d562b07033447fd2ebe1220cceb9a55e77771356
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-01-29 18:34:51 +0100

    Commented

--- bash/script1.sh
+++ bash/script1.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+# This script cclears the terminal, displays a greeting and gives information
+# about currently connected users. The two example variales are set and displayed
 
-clear
+clear							# clear terminal window
 
 echo "The script starts now."
 
-echo "Hi, $USER"
+echo "Hi, $USER"					# dollar sign is used to get content of variable
 echo
 
 echo "I will now fetch you a list of connected users:"
 echo
-w
-echo
+w							# sow who is logged on and
+echo							# what they are doing
 
 echo "I'm setting two variable now."
-COLOUR="black"
-VALUE="9"
-echo "This is a string: $COLOUR"
-echo "And this is a number: $VALUE"
+COLOUR="black"						# set a local shell variable
+VALUE="9"						# set a local shell variable
+echo "This is a string: $COLOUR"			# display content of variable
+echo "And this is a number: $VALUE"			# display content of variable
 
 echo "I'm giving you back your prompt now."
 echo
