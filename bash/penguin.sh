0b47c086326513ae39ae3fb70392a3e2b4835ffe
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmaiol.com>
2023-02-01 08:26:31 +0100

    File added

---
+++ bash/penguin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This script lets you present different menu to Tux. He will only be happy
+# when given a fish.
+
+if [ "$1" == fish ];
+then
+	echo "Hummmmmm Fish..... Tux happy!"
+else
+	echo "Tux don't like that. Tux wants fish"
+fi
