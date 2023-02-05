d9e4b740f5e53a6fd29ce72635c90d31ab395d1f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-02-05 14:15:04 +0100

    unittest class method

---
+++ bash/testcase3.py
@@ -0,0 +1,12 @@
+import unittest
+import math
+
+class MyTest(unittest.TestCase):
+	
+	@unittest.skip("Not done yet")
+	def test_add_fraction(self):
+		pass
+
+	def test_fraction_construction(self):
+		self.fail("Write this test!")
+
