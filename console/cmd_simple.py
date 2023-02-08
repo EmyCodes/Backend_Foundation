2c440e48ac0495cbc6683cc7fb5aa0d0030cf8a4
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-02-08 21:27:02 +0100

    A simple program that supports the greet command

---
+++ console/cmd_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HelloWorld(cmd.Cmd):
+	"""Simple command processor example."""
+
+	def do_greet(self, line):
+		print("Hello")
+
+	def do_EOF(self, line):
+		return True
+
+if __name__ == "__main__":
+	HelloWorld().cmdloop()
