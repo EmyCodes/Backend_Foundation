3c306bacb0b68dde90ea89639d0bd399c4886c39
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-02-08 21:36:59 +0100

    Making it look cleaner

--- console/cmd_simple.py
+++ console/cmd_simple.py
@@ -5,11 +5,19 @@ import cmd
 class HelloWorld(cmd.Cmd):
 	"""Simple command processor example."""
 
-	def do_greet(self, line):
-		print("Hello")
+	def do_greet(self, person):
+		"""greet [person]
+		Greet the named person"""
+		if person:
+			print("Hi", person)
+		else:
+			print("Hi")
 
 	def do_EOF(self, line):
 		return True
+
+	def postloop(self):
+		print
 
 if __name__ == "__main__":
 	HelloWorld().cmdloop()
