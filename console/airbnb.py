a6f1be7727e4f7bb4c91554a1fe3643d5d14fcfe
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-02-10 12:55:56 +0100

    do_EOF() added for interrupting the program

--- console/airbnb.py
+++ console/airbnb.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
 import cmd
 
 class MyConsole(cmd.Cmd):
 	pass
-
+	
+	def do_EOF(self, line):
+		"Interrupting the Program"
+		return True
 if __name__ == "__main__":
 	MyConsole().cmdloop()
