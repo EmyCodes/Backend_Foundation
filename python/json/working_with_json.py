08269ceedd830290d248c9b02d9a96e37a98a77d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-02-10 22:21:35 +0100

    working with json module

---
+++ python/json/working_with_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+# some JSON
+
+x = '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+
+y = json.loads(x)
+
+# the result is a Python DIctionary:
+print(y["age"])
+
