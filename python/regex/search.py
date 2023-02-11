02e29475cf99e260e0e5b7891deec7c8b63fc376
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-02-11 09:34:26 +0100

    Checking: If no matches are found, the value 'None' is returned

--- python/regex/search.py
+++ python/regex/search.py
@@ -3,6 +3,6 @@ import re
 
 txt = "The rain in Spain"
 x = re.search("\s", txt)
-
+y = re.search("Portugal", txt)
 print("The first white-space character is located in position: ", x.start())
-
+print(y)
