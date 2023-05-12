cc91d54e8e33aab3de598bb7640088c3c58fd3ea
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-05-12 13:48:23 +0100

    UnitConversionBot.py updated

--- Chatbot/UnitConversionBot.py
+++ Chatbot/UnitConversionBot.py
@@ -6,12 +6,25 @@ UnitBot = ChatBot('DeepChat', logical_adapters=[
 ])
 
 
+quit_commands = [
+    'quit',
+    'exit',
+    'stop',
+    'sleep',
+    'bye'
+]
 
 
 running = True
 while running:
-    print("DeepChat: What do you want to Covert? ")
+    print("DeepChat: What do you want to Convert? ")
     user = input("Me: ")
+    if str(user)=='':
+        print("DeepChat: You asked nothing...")
+        pass
+    elif user in quit_commands:
+        print("\nDeepChat: Alright, Bye. See you soon! \n")
+        break    
     try:
         bot = UnitBot.get_response(user)
         print("DeepChat: {}".format(str(bot)))
