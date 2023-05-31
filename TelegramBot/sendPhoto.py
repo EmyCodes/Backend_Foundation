d6a20100b6694cb5b244f18ea39fa79d93d995ee
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-05-31 16:14:22 +0100

    sendPhoto.py updated

---
+++ TelegramBot/sendPhoto.py
@@ -0,0 +1,27 @@
+#/usr/bin/python3
+
+import requests
+import sys
+import time
+
+token = "6225658854:AAHbEAoqdFo-gcXrEcEU5uqTYw8MjX36dqc"
+base_url = "https://api.telegram.org/bot{}/sendPhoto".format(token)
+
+photos_urls = [
+    "https://imgtr.ee/images/2023/05/31/1jolY.jpg",
+    "https://imgtr.ee/images/2023/05/31/1jolY.jpg",
+    "https://imgtr.ee/i/1jJbX",
+    "https://imgtr.ee/i/1j8R1",
+    "https://imgtr.ee/i/1jahV"
+]
+
+for url in photos_urls:
+    time.sleep(5)
+    parameters = {
+        "chat_id": "-1001579133774",
+        "photo": url,
+        "caption": "Photo {}".format(url)
+    }
+    
+    response = requests.get(base_url, data=parameters)
+    print(response.text)
