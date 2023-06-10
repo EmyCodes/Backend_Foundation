7cfaf4ad3a61e74a0799316a3c2e922064925e52
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-06-11 00:34:45 +0100

    pass semistandard style

--- JavaScript/callBack.js
+++ JavaScript/callBack.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 setTimeout(
-    function() {
-        console.log("Done!!!");
-    },
-    3000
-)
+  function () {
+    console.log('Done!!!');
+  },
+  3000
+);
 
 setTimeout(
-    () => {
-        console.log("Done!!!");
-    },
-    6000
-)
+  () => {
+    console.log('Done!!!');
+  },
+  6000
+);
