8c31690d593afcc1da59507455d8a509e4db8d67
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-06-10 23:42:55 +0100

    app.js

---
+++ JavaScript/app.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const STARTING_POKER_CHIPS = 100; // points
+const PLAYERS = 3;
+const NO_OF_STARTER_CARDS = 2;
+
+let playerOnePoints = STARTING_POKER_CHIPS;
+let playerTwoPoints = STARTING_POKER_CHIPS;
+let playerThreePoints = STARTING_POKER_CHIPS;
+
+playerOnePoints -= 50;
+playerTwoPoints -=25;
+playerThreePoints +=75;
