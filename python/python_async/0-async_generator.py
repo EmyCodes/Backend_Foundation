d8d3220edea3f31e2bedd6b6debdf2dbc3835c16
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-24 13:35:02 +0100

    feat: changed function name and parameters

---
+++ python/python_async/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Comment"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Asynchronous function"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
