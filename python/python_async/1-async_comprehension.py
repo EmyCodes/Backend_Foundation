57ae0df468e1e579f3b460755bf42459f53043e0
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-24 13:43:19 +0100

    feat: 1-async_comprehension.py

---
+++ python/python_async/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import asyncio
+
+
+async_generator = __import__(0-async_comprehension).async_generator
+
+
+async def async_comprehension():
+    comprehesion = []
+    for i in async_generator():
+        comprehesion.append(i)
+    return comprehesion
