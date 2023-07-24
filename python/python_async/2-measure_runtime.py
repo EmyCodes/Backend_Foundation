3bb096035b41b4e57d9d317fcbb5f951b88a23ca
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-24 14:27:03 +0100

    feat: added and removed some keywords

--- python/python_async/2-measure_runtime.py
+++ python/python_async/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Comment"""
 import asyncio
-from typing import float
+# from typing import float
 import time
 
 
@@ -9,7 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     start_time = time.time()
-    asyncio.gather(*[async_comprehension async for i range(4)])
+    await asyncio.gather(*[async_comprehension() for i in range(4)])
     end_time = time.time()
     time_taken = end_time - start_time
     return time_taken
