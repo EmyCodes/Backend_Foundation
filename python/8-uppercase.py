beb1727dbe31f5d455c6b7a6c802e725bb9060fe
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-31 13:23:37 +0100

    feat: defined conditional statement

---
+++ python/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function that prints a string in uppercase
+followed by a new line"""
+
+
+def uppercase(str):
+    """function that prints a string in uppercase
+    followed by a new line."""
+    for i in str:
+        # if (ord(i) >= 97) and (ord(i) <= 122):
+        if (i >= 'a') and (i <= 'z'):
+            ascii_diff = 97 - 65
+            converted_i = ord(i) - ascii_diff
+            # print(onverted_i)
+            convert_back = chr(converted_i)
+            print(convert_back, end=" ")
+    print()
