8dd830ece631b18f3129426de9527e3d60cf45ce
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-31 13:36:53 +0100

    fix: fixed syntax errors

--- python/9-print_last_digit.py
+++ python/9-print_last_digit.py
@@ -6,12 +6,10 @@ def print_last_digit(number):
     """function that prints the last digit of a number."""
     # Make number absolute
     number = abs(number)
-    if (number % (3*5)) = 0:
+    if (number % (3*5)) == 0:
         number = number % (3*5)
     elif number % 3 == 0:
         number = number % 3
     elif number % 5 == 0:
         number = number % 5
-    else:
-        continue
     return number
