68ff30ea83a926439e7315962e95229aa3e212a7
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-02 00:06:11 +0100

    feat: file adds two numbers together

---
+++ python/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Add function"""
+
+from add_0 import add
+
+
+def add(a, b):
+    """Defining function"""
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b))
