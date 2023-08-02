884b1fc6fff50e2de84e1534638b94dc5b669787
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-02 01:04:51 +0100

    feat: 2-args.py

---
+++ python/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 1:
+        print("{} arguments.".format(0))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("{}: {}".format(len(argv) -1, argv))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
