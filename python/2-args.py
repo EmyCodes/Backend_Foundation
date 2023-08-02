c83bb73c25722221ca0a2ae5656b2b45fdadd9da
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-02 01:12:08 +0100

    fix: corrected the code to give accurate result

--- python/2-args.py
+++ python/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(0))
     elif len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
-        print("{}: {}".format(len(argv) -1, argv))
+        print("{}: {}".format(len(argv) - 1, argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
