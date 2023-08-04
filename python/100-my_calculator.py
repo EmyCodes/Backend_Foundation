23145be7d4a331134afa41d69ad28eb9a72538ad
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-04 02:14:57 +0100

    chore: corrected typo error on the operators

--- python/100-my_calculator.py
+++ python/100-my_calculator.py
@@ -24,8 +24,8 @@ Available operators: +, -, * and /")
             if argv[2] == '+':
                 print("{} + {} = {}".format(a, b, add(a, b)))
             elif argv[2] == '-':
-                print("{} + {} = {}".format(a, b, sub(a, b)))
+                print("{} - {} = {}".format(a, b, sub(a, b)))
             elif argv[2] == '/':
-                print("{} + {} = {}".format(a, b, div(a, b)))
+                print("{} / {} = {}".format(a, b, div(a, b)))
             elif argv[2] == '*':
-                print("{} + {} = {}".format(a, b, mul(a, b)))
+                print("{} * {} = {}".format(a, b, mul(a, b)))
