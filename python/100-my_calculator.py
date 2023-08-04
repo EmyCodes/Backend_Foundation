46401026e6d081682b0f8342cc6c7fa6fcdd7eab
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-04 02:49:17 +0100

    chore: added exit() method

--- python/100-my_calculator.py
+++ python/100-my_calculator.py
@@ -6,17 +6,19 @@ calculator_1.py and handles basic operations.
 
 
 if __name__ == "__main__":
-    from sys import argv
+    from sys import argv, exit
     from calculator_1 import add, sub, div, mul
 
     operators = ['+', '-', '*', '/']
 
     if len(argv) != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
     else:
         if argv[2] not in operators:
             print("Unknown operator. \
 Available operators: +, -, * and /")
+            exit(1)
 
         a = int(argv[1])
         b = int(argv[3])
