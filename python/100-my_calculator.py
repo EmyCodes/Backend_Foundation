8267c1b74c2b8987a3cbec60f5972c81fb46d356
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-04 02:44:41 +0100

    feat: handled operator operation

--- python/100-my_calculator.py
+++ python/100-my_calculator.py
@@ -7,25 +7,27 @@ calculator_1.py and handles basic operations.
 
 if __name__ == "__main__":
     from sys import argv
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, div, mul
 
     operators = ['+', '-', '*', '/']
+
     if len(argv) != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        pass
     else:
         if argv[2] not in operators:
             print("Unknown operator. \
 Available operators: +, -, * and /")
-        else:
-            a = int(len(argv[1]))
-            b = int(len(argv[3]))
 
-            if argv[2] == '+':
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            elif argv[2] == '-':
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            elif argv[2] == '/':
-                print("{} / {} = {}".format(a, b, div(a, b)))
-            elif argv[2] == '*':
-                print("{} * {} = {}".format(a, b, mul(a, b)))
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if argv[2] == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif argv[2] == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        elif argv[2] == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        else:
+            pass
