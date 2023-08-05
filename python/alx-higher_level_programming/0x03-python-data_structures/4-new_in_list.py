83db7df7a7c321f299daa9fdba3681df188ebe14
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-05 10:17:20 +0100

    feat: 4-new_in_list.py

---
+++ python/alx-higher_level_programming/0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that replaces an element in a list at a
+specific position without modifying the original list (like in C).
+"""
+
+
+def new_in_list(my_list, idx, element):
+    """function that replaces an element in a list at a
+    specific position without modifying the original list (like in C)
+    """
+    if (idx < 0) or (idx >= len(my_list)):
+        return my_list
+    else:
+        new_list = my_list[:]
+        # or
+        # new_list = my_list.copy()
+        return new_list
