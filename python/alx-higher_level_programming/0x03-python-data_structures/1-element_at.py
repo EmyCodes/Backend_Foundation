9bac13467b47ee735e4493a91311bd985ee288f3
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-05 09:40:07 +0100

    feat: 1-element_at.py

---
+++ python/alx-higher_level_programming/0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that retrieves an element from a list like in C.
+"""
+
+def element_at(my_list, idx):
+    """
+    function that retrieves an element from a list like in C.
+    """
+    if (idx < 0) or (idx > len(my_list) -1):
+    # if (idx < 0) 0r (idx >= len(my_list)):
+        return None
+    else:
+        return my_list[idx]
