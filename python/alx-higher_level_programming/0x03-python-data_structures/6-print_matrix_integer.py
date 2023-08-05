f0aacd837fa869a379f1f6c794caac4a9c1e5041
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-05 10:52:50 +0100

    fix: resolved SyntaxError on line 9 by closing bracket

--- python/alx-higher_level_programming/0x03-python-data_structures/6-print_matrix_integer.py
+++ python/alx-higher_level_programming/0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
     """function that prints a matrix of integers."""
     for i in matrix:
         for j in i:
-            print("{:d}".format(j, end=" ")
+            print("{:d}".format(j, end=" "))
