a24121a90760fdb8ca287aa90fc528d3affbb684
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-05 10:34:29 +0100

    feat: resolved to give expected result

--- python/alx-higher_level_programming/0x03-python-data_structures/5-no_c.py
+++ python/alx-higher_level_programming/0x03-python-data_structures/5-no_c.py
@@ -9,7 +9,7 @@ def no_c(my_string):
     function that removes all characters c and C from a string.
     """
     if ("C" in my_string) or ("c" in my_string):
-        my_string.strip("C").strip("c")
+        my_string = my_string.strip("C").strip("c")
         return my_string
     else:
         return my_string
