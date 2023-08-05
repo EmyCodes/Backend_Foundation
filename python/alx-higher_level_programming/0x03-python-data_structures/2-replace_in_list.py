b77fcd911a00b0c2a71c05d48e699dde7f7946aa
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-05 09:51:12 +0100

    feat: 2-replace_in_list.py

---
+++ python/alx-higher_level_programming/0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that replaces an element of a list
+at a specific position (like in C)."""
+
+
+def replace_in_list(my_list, idx, element):
+    """
+    function that replaces an element of a list at a
+    specific position
+    """
+    if (idx < 0) or (idx >= len(my_list)):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
