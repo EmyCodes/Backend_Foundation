f4dc1f740083a9f3779fcd5bc8f0aae308f31026
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-05 10:00:20 +0100

    feat: wrote this script and changed mode

---
+++ python/alx-higher_level_programming/0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that prints all integers of a list, in reverse order"""
+
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    function that prints all integers of a list, in reverse order.
+    """
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i])
