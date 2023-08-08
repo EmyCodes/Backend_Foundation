8f04bd5dae29f2b7c96419c7d0df30b8446fb856
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-08 22:43:06 +0100

    feat: resolved to give desired output

--- python/alx-higher_level_programming/0x04-python-more_data_structures/0-square_matrix_simple.py
+++ python/alx-higher_level_programming/0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -9,9 +9,10 @@ def square_matrix_simple(matrix=[]):
     function that computes the square value of all integers of a matrix.
     """
     new_matrix = matrix[:]
+    new_matrix_ = []
     for row in new_matrix:
         rows = []
         for column in row:
-            rows.append(column*2)
-        return rows
-
+            rows.append(column ** 2)
+        new_matrix_.append(rows)
+    return new_matrix_
