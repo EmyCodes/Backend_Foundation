d6b5ef36355e1efdc3bd13c50d23baa936fc1ee8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-11 10:12:01 +0100

    feat: updating

--- python/flask_.py
+++ python/flask_.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home(name="EmyCodes"):
-    return f"<h1>Hello {name}</h1>"
+def home():
+    return "Hello, Welcome to my Page!"
+
+@app.route('/<name>')
+def user(name="EmyCodes"):
+    return f"<h1>Hello {name}</h1>!"
+
+def admin():
+    return return(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run()
