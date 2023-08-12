f6d257506b35c4b56d4afa987ce7d6dc38c74e0c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-13 00:43:12 +0100

    feat: changed some parameters

--- python/pdf2docx/convert_pdf2_docx.py
+++ python/pdf2docx/convert_pdf2_docx.py
@@ -6,8 +6,8 @@ from sys import argv
 
 # check
 def convert_pdf_to_docx(original=argv[1], converted=argv[2]):
-        to_convert = f"./{argv[1]}"
-        converted_to = f"./{argv[2]}"
+        to_convert = f"./{original}"
+        converted_to = f"./{converted}"
         
         parse(to_convert, converted_to)
 
