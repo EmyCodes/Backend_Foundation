c9058d59550bd7b0c98f406b295bda09aa5bacc2
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-13 00:50:12 +0100

    style: passed PEP8 style

--- python/pdf2docx/convert_pdf2_docx.py
+++ python/pdf2docx/convert_pdf2_docx.py
@@ -9,8 +9,9 @@ def convert_pdf_to_docx(original=argv[1], converted=argv[2]):
     """This function covert pdf file to docx file"""
     to_convert = f"./{original}"
     converted_to = f"./{converted}"
-        
+    
     parse(to_convert, converted_to)
+
 
 if __name__ == "__main__":
     convert_pdf_to_docx()
