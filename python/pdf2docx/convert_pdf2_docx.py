14d322f2c672c6475bf5a3191e7347fbc41e13ed
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-13 00:49:28 +0100

    doc: added documentation the function

--- python/pdf2docx/convert_pdf2_docx.py
+++ python/pdf2docx/convert_pdf2_docx.py
@@ -6,9 +6,11 @@ from sys import argv
 
 # check
 def convert_pdf_to_docx(original=argv[1], converted=argv[2]):
+    """This function covert pdf file to docx file"""
     to_convert = f"./{original}"
     converted_to = f"./{converted}"
         
     parse(to_convert, converted_to)
 
-convert_pdf_to_docx()
+if __name__ == "__main__":
+    convert_pdf_to_docx()
