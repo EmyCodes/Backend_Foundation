b8b90f089e675c494ab219042554434923f2cc1b
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-17 13:48:08 +0100

    feat: resolving potential errors

--- python/isprime.py
+++ python/isprime.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
-def isprime(n):
+def isprime(n: int) -> str:
     """This function detcts prime number"""
     if (n % n == 0) and (n % 2 != 1):
         return "{} is prime".format(n)
     else:
         return "{} is not prime".format(n)
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    isprime(argv[1])
