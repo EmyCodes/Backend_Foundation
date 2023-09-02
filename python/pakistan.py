2dc53be1f5a32b031f427f993fdf01bda4a2cda8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-09-02 11:08:58 +0100

    feat: converted string to list

--- python/pakistan.py
+++ python/pakistan.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+"""
+This script converts string to list whose elements are in lowercase
+"""
 
-string = "Pakistan"
-string = string.lower()
-new_string = []
-for char in string:
-    new_string.append(char)
-print(new_string)
+from sys import argv, exit
+
+if len(argv) == 3:
+    string = argv[2]
+    string = string.lower()
+    new_string = []
+    for char in string:
+        new_string.append(char)
+    print(new_string)
+elif len(argv) == 2:
+    string = argv[1]
+    string = string.lower()
+    new_string = []
+    for char in string:
+        new_string.append(char)
+    print(new_string)
+else:
+    exit(1)
