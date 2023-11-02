60aa3d802a4df68b02c605a0ffe2016afd08222f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-02 22:49:05 +0100

    feat: working with callback

---
+++ JavaScript/Callbacks/0-callback.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const myNum = [1, -1, 2, -2, 3, -3, 4, -100, 5];
+
+const myFilter = removeNNeg(myNum, (x) => x >= 0);
+
+function removeNNeg (myNum, callback) {
+  const myArray = [];
+  for (const x of myNum) {
+    if (callback(x)) {
+      myArray.push(x);
+    }
+  }
+}
