9154af9112f29485203ba10dbd93c0539548c711
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-02 22:55:04 +0100

    feat: changed mode and addedd return operator

--- JavaScript/Callbacks/0-callback.js
+++ JavaScript/Callbacks/0-callback.js
@@ -4,11 +4,13 @@ const myNum = [1, -1, 2, -2, 3, -3, 4, -100, 5];
 
 const myFilter = removeNNeg(myNum, (x) => x >= 0);
 
-function removeNNeg (myNum, callback) {
+function removeNNeg(myNum, callback) {
   const myArray = [];
   for (const x of myNum) {
     if (callback(x)) {
       myArray.push(x);
     }
   }
+  return myArray;
 }
+console.log(myFilter);
