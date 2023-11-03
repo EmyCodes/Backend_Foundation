37a4942a7dc01b58243530f6aa541bd54b0efed5
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-03 10:46:50 +0100

    style: pass semistandard style

--- JavaScript/Callbacks/3-promise.js
+++ JavaScript/Callbacks/3-promise.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-console.log("Wait 5 seconds (5000 milliseconds) for this page to change.");
+console.log('Wait 5 seconds (5000 milliseconds) for this page to change.');
 
-setTimeout(() => 
-	console.log("I love you!!"), 
-	5000);
-
+setTimeout(() =>
+  console.log('I love you!!'),
+5000);
