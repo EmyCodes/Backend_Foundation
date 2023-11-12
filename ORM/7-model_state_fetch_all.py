df3d1f1b91747d8def7f94992a839cddbaf2e347
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-13 00:53:11 +0100

    feat: update Task 7

--- ORM/7-model_state_fetch_all.py
+++ ORM/7-model_state_fetch_all.py
@@ -10,10 +10,13 @@ if __name__ == "__main__":
     # Create Engine
     engine = create_engine(
         f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
-    Session = sessionmaker()
-    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     states = session.query(State).order_by(State.id)
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
+    session.close()
