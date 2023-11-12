d77ed7e0f6cb77bc31441565fe8fb4506de523d8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-12 23:50:34 +0100

    style: pass PEP8

--- ORM/model_state.py
+++ ORM/model_state.py
@@ -10,4 +10,4 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(150),nullable=False)
+    name = Column(String(150), nullable=False)
