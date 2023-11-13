1ff1010635c9ebdb4beeee874f16dabff4e069cf
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-13 10:49:58 +0100

    feat: Task 8

---
+++ ORM/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that contains the class State"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    # Create Engine
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+    Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state is None:
+
+	    print("Nothing")
+    else:
+	    print("{}: {}".format(state.id, state.name))
+    
+    # Close Session
+    session.close()
